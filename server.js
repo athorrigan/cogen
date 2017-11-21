7e3946f347eaa636d3f7e0fd5cd832553b0d6394
Cory Withers <javecantrell@gmail.com>
2017-11-21 10:05:55 -0500

    Test port.

--- server.js
+++ server.js
@@ -448,6 +448,7 @@ app.get('/sidebar/:showSidebar', (req, res) => {
 
 // Link to compile and serve the pdf of the course
 app.get('/pdf/:title', (req, res) => {
+    console.log(server.address().port);
     const
         // External library for converting html to pdf
         htmlPdf = require('html-pdf'),
